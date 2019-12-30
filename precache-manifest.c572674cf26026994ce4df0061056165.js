c51675fc5b94ab93d9b5f3f5750824cc1cc9d362
aim_akonya <aim.akonya8@gmail.com>
2019-12-30 15:41:39 +0300

    Updates

---
+++ precache-manifest.c572674cf26026994ce4df0061056165.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb0b9ce9f2e9009bb8013d3e67a68966",
+    "url": "/aim-portfolio/index.html"
+  },
+  {
+    "revision": "623d652b66429eea548b",
+    "url": "/aim-portfolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "aee95c96c9f1221ed971",
+    "url": "/aim-portfolio/static/js/2.c85f9b24.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/aim-portfolio/static/js/2.c85f9b24.chunk.js.LICENSE"
+  },
+  {
+    "revision": "623d652b66429eea548b",
+    "url": "/aim-portfolio/static/js/main.2f74ee3c.chunk.js"
+  },
+  {
+    "revision": "c0783c5a78bd7f8f8160",
+    "url": "/aim-portfolio/static/js/runtime-main.6fa37092.js"
+  }
+]);
